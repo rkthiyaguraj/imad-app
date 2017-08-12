38ebe9cda72bdcf11c957bca0397a58d9f80392b
rkthiyaguraj <30825801+rkthiyaguraj@users.noreply.github.com>
2017-08-12 02:06:31 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ app.get('/ui/index.html', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
 app.get('/ui/main.ja', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
