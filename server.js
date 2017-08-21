7d27077723ea1c0f2de3f81591513824126919f1
rkthiyaguraj <30825801+rkthiyaguraj@users.noreply.github.com>
2017-08-20 22:17:36 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,27 +33,8 @@ app.get('/test.db',function(req,res){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-}
-);
-app.get('/article-two',function(req,res){
-    res.send('article two responded');
-}
-);
-app.get('/article-three',function(req,res){
-    res.send('article three responded');
-}
-);
-
-
-
-        }
         
-        }
-    })
-}))
+        
 app.get('/ui/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
