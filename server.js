de299d61d0ecf4cdb45ba1dbaadcc49a9af8441c
rkthiyaguraj <30825801+rkthiyaguraj@users.noreply.github.com>
2017-08-20 22:10:10 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config={
      database : 'rkthiyaguraj',
      host : 'db.imad.hasura_app.io',
      port : '5432',
-     password : process.env.DB_PASSWORD
+     password : process.env.DB_PASSWORD,
 };
 }
 var app = express();
